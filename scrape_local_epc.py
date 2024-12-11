8e6c97ec95702ea5d7d053e247aa9bf50eaec177
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2024-12-11 10:50:35 +0000

    Fix: Correctly parse address

--- scrape_local_epc.py
+++ scrape_local_epc.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 class EnergyCertificateScraper:
-    def __init__(self, postcode):
+    def __init__(self, address, postcode):
         self.postcode = postcode.replace(" ", "%20")
+        self.address = address
         self.url = f"https://find-energy-certificate.service.gov.uk/find-a-certificate/search-by-postcode?postcode={self.postcode}"
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
@@ -27,28 +28,41 @@ class EnergyCertificateScraper:
             head = [tr.find('th').text.strip()]
             cells = [td.text.strip() for td in tr.find_all('td')]
             head += cells
+            
+            
+            
             if head[0] != 'Address':
-                rows.append(head)
+                if head[0] == self.address:
+                    self.epc = head[1]
+                else: 
+                    rows.append(head)
         self.df = pd.DataFrame(rows, columns=['address', 'rating', 'expired'])
     
-    def remove_address(self, address):
-        self.adress = address
-        self.df = self.df[self.df['address'] != address]
+    def average_rating(self):
         
-        # Get your data
-        user = self.df[self.df['address'] == address]
-        return user
-        if not user.empty:
-            self.epc =  user['rating'].values[0]
-            print(f"Rating: {self.epc}")
-            return self.epc
+        rating_map = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7}
+        inverse_rating_map = {v: k for k, v in rating_map.items()}
+        
+        # Convert ratings to numerical values
+        self.df['rating_num'] = self.df['rating'].map(rating_map)
+        
+        # Calculate the median of the numerical ratings
+        median_num = self.df['rating_num'].median()
+        
+        # Convert the numerical median back to a letter
+        median_rating = inverse_rating_map[median_num]
+        
+        return median_rating
+
     
     def return_df(self):
         return self.df
+    
+    def return_epc(self):
+        return self.epc
 
-postcode = "YO105BZ"
-scraper = EnergyCertificateScraper(postcode)
+
+scraper = EnergyCertificateScraper('5, Barbican Mews, YORK, YO10 5BZ', "YO105BZ")
 scraper.parse_table()
-
-scraper.remove_address("1, Barbican Mews, YORK, YO10 5BZ")
-
+avg_rating = scraper.average_rating()
+print(f"Average rating: {scraper.average_rating()},\n Your rating: {scraper.return_epc()}")
